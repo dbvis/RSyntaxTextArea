58a0dfa75e8960d8d6f35503bc9c8f7bc2913850
Peer Törngren <reg-github@p2r.se>
2023-01-31 12:59:32 +0100

    merge and modify performance optimization from first branch for issue 487
    1. Instead of calling charWidth() for each character, increment a counter until token end or a tab character is encountered
    2. Recursively analyze the buffered text using half segments until offset is found

--- RSyntaxTextArea/src/test/java/org/fife/ui/rsyntaxtextarea/TokenImplTest.java
+++ RSyntaxTextArea/src/test/java/org/fife/ui/rsyntaxtextarea/TokenImplTest.java
@@ -9,6 +9,12 @@ package org.fife.ui.rsyntaxtextarea;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+import javax.swing.text.StringContent;
+import java.awt.*;
+
 
 /**
  * Unit tests for the {@link TokenImpl} class.
@@ -192,5 +198,89 @@ class TokenImplTest {
 		char[] ch = "for".toCharArray();
 		TokenImpl token = new TokenImpl(ch, 0, 2, 0, TokenTypes.RESERVED_WORD, 0);
 		Assertions.assertFalse(token.isComment());
+	}
+
+	@Test
+    void testGetListOffset() {
+		String text = "0123456789";
+		float xOffset = 0;
+
+		// ideal click before character (token is first one the line)
+		assertGetListOffset(text, 0, xOffset, 0f);
+		assertGetListOffset(text, 1, xOffset, 10f);
+		assertGetListOffset(text, 2, xOffset, 20f);
+		assertGetListOffset(text, 3, xOffset, 30f);
+		assertGetListOffset(text, 4, xOffset, 40f);
+		assertGetListOffset(text, 5, xOffset, 50f);
+		assertGetListOffset(text, 6, xOffset, 60f);
+		assertGetListOffset(text, 7, xOffset, 70f);
+		assertGetListOffset(text, 8, xOffset, 80f);
+		assertGetListOffset(text, 9, xOffset, 90f);
+
+		// with offset (token is not first on the line)
+		xOffset = 100;
+		assertGetListOffset(text, 0, xOffset, 100f);
+		assertGetListOffset(text, 1, xOffset, 110f);
+		assertGetListOffset(text, 2, xOffset, 120f);
+		assertGetListOffset(text, 3, xOffset, 130f);
+		assertGetListOffset(text, 4, xOffset, 140f);
+		assertGetListOffset(text, 5, xOffset, 150f);
+		assertGetListOffset(text, 6, xOffset, 160f);
+		assertGetListOffset(text, 7, xOffset, 170f);
+		assertGetListOffset(text, 8, xOffset, 180f);
+		assertGetListOffset(text, 9, xOffset, 190f);
+
+		// click on character ("truncate")
+		xOffset = 0;
+		assertGetListOffset(text, 0, xOffset, 5f);
+		assertGetListOffset(text, 0, xOffset, 5.9f);
+		assertGetListOffset(text, 9, xOffset, 95f);
+		assertGetListOffset(text, 9, xOffset, 95.9f);
+
+		// click close to end of character ("round"") - NOT IMPLEMENTED
+		xOffset = 0;
+//		assertGetListOffset(1, 5.01f);
+//		assertGetListOffset(-1, 95.01f);
+	}
+
+	private static void assertGetListOffset(String text, int expected, float x0, float x) {
+		int tokenStartOffset = 0;
+		int tokenEndOffset = text.length();
+		FontMetrics fm = new MyFontMetrics(10f);
+
+		TokenImpl token = new TokenImpl();
+		int actual = token.getListOffset(fm, text.toCharArray(), tokenStartOffset, tokenEndOffset, x0, x);
+		String msg = String.format("Clicked=%s - Expected: \"%s\"[%s]='%s' | Actual: %s='%s'", x, text, expected, (expected < 0 ? null : text.substring(expected, expected + 1)), actual, (actual < 0 || actual >= text.length() ? null : text.substring(actual, actual + 1)));
+		Assertions.assertEquals(expected, actual, msg);
+	}
+
+	private static class MyFontMetrics extends FontMetrics {
+		private final float averageCharacterWidth;
+		private final boolean proportionalFont;
+
+		protected MyFontMetrics(float fixedCharacterWidth) {
+			this(fixedCharacterWidth, false);
+		}
+
+		protected MyFontMetrics(float averageCharacterWidth, boolean proportionalFont) {
+			super(null);
+			this.averageCharacterWidth = averageCharacterWidth;
+			this.proportionalFont = proportionalFont;
+		}
+
+		@Override
+		public int charWidth(char ch) {
+			return proportionalFont ? randomize(averageCharacterWidth) : (int) averageCharacterWidth;
+		}
+
+		@Override
+		public int charsWidth(char[] data, int off, int len) {
+			float w = len * averageCharacterWidth;
+			return proportionalFont ? randomize(w) : (int) w;
+		}
+
+		private int randomize(float w) {
+			return (int) (w + 1/Math.random());
+		}
 	}
 }
